479affd247a9a9a3718f8b3925202c9a360f2eed
wangguolong <2192344069@qq.com>
2022-06-18 16:37:20 +0800

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println(Math.round(11.5));
+        System.out.println(Math.round(11.5));
         System.out.println(Math.round(-11.5));
     }
 }
